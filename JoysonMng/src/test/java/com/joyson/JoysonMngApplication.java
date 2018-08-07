76252495c03a81654dc2c80cf6406a468f6851c6
JP\changjis <Jisu.Chang@jp.joysonsafety.com>
2018-08-07 17:46:33 +0900

    20180807 Takata

--- JoysonMng/src/test/java/com/joyson/JoysonMngApplication.java
+++ JoysonMng/src/test/java/com/joyson/JoysonMngApplication.java
@@ -1,20 +1,16 @@
 package com.joyson;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.joyson.domain.Customer;
+import com.joyson.repository.CustomerRepository;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -22,12 +18,14 @@ import com.joyson.domain.Customer;
 public class JoysonMngApplication implements CommandLineRunner{
 
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 	
 	/*
 	@Autowired
 	CustomerService customerService;
 	*/
+	
+	static final Logger log = LoggerFactory.getLogger(JoysonMngApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JoysonMngApplication.class, args);
@@ -37,25 +35,10 @@ public class JoysonMngApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		/*
-		customerService.save(new Customer(1, "Nobita", "Nobi"));
-		customerService.save(new Customer(2, "Takeshi", "Goda"));
-		customerService.save(new Customer(3, "Suneo", "Honekawa"));
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		log.info(created + " is created!");
 		
-		customerService.findAll().forEach(System.out::println);
-		*/
-		
-		
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 1);
-		
-		
-		
-		Customer result = jdbcTemplate.queryForObject(sql, param, (rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name")));
-		
-		
-		System.out.println("result=" + result);
-		
+		customerRepository.findAll().forEach(System.out::println);
+
 	}
 }
